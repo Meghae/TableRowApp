3e3d68c47ffa6af9eb824f09f8abdac17d68812c
Admin <admin@admins-macbook-pro.local>
2021-11-27 01:48:53 +0530

    Initial Commit

---
+++ TableRowImages/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableRowImages
+//
+//  Created by Admin on 27/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
